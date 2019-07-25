a11e368d5858a87fc27e3dc6490a3ab19f5c9cb5
NischayaSharma <nischaya1703@gmail.com>
2019-07-25 17:49:20 +0530

    fix: Queue underflow condition after deleteing all elements

--- Queues/CircularQueue.java
+++ Queues/CircularQueue.java
@@ -1,22 +1,20 @@
-class Queue {
+class CircularQueue {
     private int rear = -1, front = -1, max;
-    private int[] arr = new int[10000];
+    private int[] arr = new int[rear];
 
-    Queue(int maximumElements) {
+    Queue(int maximumElements){
         max = maximumElements;
-        for (int i = 0; i < maximumElements; i++) {
-            arr[i] = -1;
+        for(int i=0 ; i<maximumElements ; i++){
+            arr[i]=-1;
         }
     }
 
     void qInsert(int data) {
-        if (rear < max - 1) {
+        if (rear < max) {
             if (rear == -1 && front == -1) {
-                rear += 1;
                 front += 1;
-            } else {
-                rear += 1;
             }
+            rear += 1;
             arr[rear] = data;
         } else {
             System.out.println("The Queue is full(Overflow)");
@@ -24,20 +22,16 @@ class Queue {
     }
 
     void qDelete() {
-        if (!(front == -1)) {
+        if (rear == -1 && front == -1) {
             arr[front] = -1;
             front += 1;
-            // System.out.println(front+" r:"+rear);
         } else {
-            System.out.println("The Queue is Empty(Underflow)");
-        }
-        if (front>rear+1){
             System.out.println("The Queue is Empty(Underflow)");
         }
     }
 
     void display() {
-        for (int i = front; i <= rear; i++) {
+        for (int i = front; i < rear; i++) {
             System.out.print(arr[i] + ", ");
         }
         System.out.println();
